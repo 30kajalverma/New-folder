203697fd27f79e3ab6ac0fcb12bcaef9a149f271
30kajalverma <30kajalverma@gmail.com>
2025-06-19 14:54:57 +0530

    new file added 02_negative_slicing.py

---
+++ 02_negative_slicing.py
@@ -0,0 +1,24 @@
+name = "Harry"
+
+print(name[0:3])
+
+print(name[-4: -1]) #negative ko ap positive corressponding m convert kr lo
+print(name[1:4])
+
+print(name[ :4]) # is same as print(name[0:4])
+print(name[1:]) # is same as print(name[1:5])
+print(name[1:5])
+
+# slicing with skip value
+word = "amazing"
+
+word[1:6:2]
+
+# other advanced slicing techniques:
+a= "amazing"
+a [:7]
+a [0:]
+
+
+
+
